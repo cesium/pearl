f1c7a955c912d242f63b25d8d7755fae95fe292e
gxnca <fl11gg20@gmail.com>
2025-11-27 18:53:03 +0000

    fix: warnings

--- lib/pearl_web/live/app/games_live/components/game_card.ex
+++ lib/pearl_web/live/app/games_live/components/game_card.ex
@@ -4,15 +4,21 @@ defmodule PearlWeb.App.GamesLive.Components.GameCard do
   """
   use PearlWeb, :component
 
+  attr :name, :string, required: true
+  attr :path, :string, required: true
+  attr :desc, :string, required: true
+  attr :icon, :string, required: true
+
+
   def game_card(assigns) do
     ~H"""
     <.link
-      patch={~p"/app/games/#{@path}"}
+      patch={"/app/games/#{@path}"}
       class="flex flex-col h-full group gap-6 rounded-2xl border border-white/10 bg-black/20 p-8 backdrop-blur-md transition-all duration-500 hover:border-white/20 hover:bg-black/30 hover:shadow-2xl hover:shadow-primary/10"
     >
       <img src={~p"/images/icons/#{@icon}"} class="size-16 sm:size-24 invert" />
       <div class="gap-2">
-        <p class="text-2xl font-bold">{@game}</p>
+        <p class="text-2xl font-bold">{@name}</p>
         <p class="text-white/60">{@desc}</p>
       </div>
       <div class="inline-flex items-center gap-2 group-hover:gap-3 transition-all duration-300">

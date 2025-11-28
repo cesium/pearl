dd750ef9d7dfbe02f89344ce6d572e3df4420610
Enrico Prazeres <esprazj.dev@gmail.com>
2025-11-28 11:58:53 +0000

    feat: tickets page

--- lib/pearl_web/live/landing/tickets_live/components/card.ex
+++ lib/pearl_web/live/landing/tickets_live/components/card.ex
@@ -1,3 +1,52 @@
 defmodule PearlWeb.Landing.TicketsLive.Components.Card do
+  @moduledoc """
+  Tickets component.
+  """
+  use PearlWeb, :component
 
+  def card(assigns) do
+    ~H"""
+    <div class="w-full h-full flex flex-col justify-between bg-white overflow-hidden">
+      <div
+        class="relative flex flex-col text-white p-4 bg-cover bg-center"
+        style="background-image: url('/images/tickets/background.png')"
+      >
+        <div class="absolute bg-black/30 inset-0"></div>
+        <span class="relative font-extrabold text-2xl">{assigns.ticket_type.name}</span>
+        <span class="relative font-extrabold text-2xl">€{assigns.ticket_type.price}</span>
+      </div>
+      <div class="flex flex-col text-black p-8 gap-6 h-full justify-between">
+        <div class="space-y-2">
+          <span class="font-extrabold text-xl leading-tight block">
+            {assigns.ticket_type.description}
+          </span>
+          <div class="h-1 w-12 bg-wine"></div>
+        </div>
+        <ul class="space-y-3 text-gray-700">
+          <li class="flex items-start gap-3">
+            <span class="text-wine mt-1">•</span>
+            <span>Backstage access</span>
+          </li>
+          <li class="flex items-start gap-3">
+            <span class="text-wine mt-1">•</span>
+            <span>Private dinner with speakers</span>
+          </li>
+          <li class="flex items-start gap-3">
+            <span class="text-wine mt-1">•</span>
+            <span>Exclusive merch</span>
+          </li>
+        </ul>
+      </div>
+      <div class="flex justify-center place-items-center bg-wine">
+        <.button
+          class="rounded-none bg-transparent! text-white! px-1 py-5 cursor-pointer w-full h-full group flex justify-center items-center gap-2"
+          phx-value-ticket_type_id={assigns.ticket_type.id}
+        >
+          <span class="text-sm group-hover:-translate-x-0.5 transition-all">Get this ticket</span>
+          <.icon name="hero-arrow-right" class="h-4 w-4 group-hover:translate-x-0.5 transition-all" />
+        </.button>
+      </div>
+    </div>
+    """
+  end
 end
--- lib/pearl_web/live/landing/tickets_live/index.ex
+++ lib/pearl_web/live/landing/tickets_live/index.ex
@@ -1,14 +1,19 @@
 defmodule PearlWeb.Landing.TicketsLive.Index do
   use PearlWeb, :landing_view
 
-    on_mount {PearlWeb.VerifyFeatureFlag, "tickets_enabled"}
+  on_mount {PearlWeb.VerifyFeatureFlag, "tickets_enabled"}
+
+  import PearlWeb.Landing.TicketsLive.Components.Card
+
+  alias Pearl.TicketTypes
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    ticket_types = TicketTypes.list_ticket_types()
+
+    {:ok, socket |> assign(:ticket_types, ticket_types)}
   end
 
   def handle_params(_unsigned_params, _uri, socket) do
     {:noreply, socket}
   end
-
 end

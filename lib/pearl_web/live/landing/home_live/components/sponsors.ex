d6c13c52106ba235985d05f8a5ec6d28be9fec64
gxnca <fl11gg20@gmail.com>
2025-12-06 21:52:58 +0000

    refactor: sponsors section

--- lib/pearl_web/live/landing/home_live/components/sponsors.ex
+++ lib/pearl_web/live/landing/home_live/components/sponsors.ex
@@ -6,40 +6,47 @@ defmodule PearlWeb.Landing.HomeLive.Components.Sponsors do
 
   def sponsors(assigns) do
     ~H"""
-    <div class="flex items-center justify-center flex-col">
-      <h2 class="font-terminal uppercase flex justify-center py-10 text-center text-4xl xs:text-5xl sm:text-6xl md:text-7xl">
-        {gettext("Our amazing sponsors")}
-      </h2>
-      <%= for tier <- @tiers do %>
-        <.sponsor_segment tier={tier.name} sponsors={tier.companies} />
-      <% end %>
+    <div class="flex items-center justify-center flex-col py-14.5 bg-[#EFEFED]">
+      <div class="flex flex-col gap-5 items-center text-black">
+        <h2 class="flex font-semibold justify-center text-center text-3xl w-[580px]">
+          {gettext("Um elenco de empresas que abre portas e janelas")}
+        </h2>
+        <p class="lg:px-52.5 text-center">
+          As empresas incríveis que nos patrocinam são quem possibilita o ENEI. Durante todo o evento, vais poder encontrá-las e falar com os seus representantes para as conheceres melhor.
+        </p>
+      </div>
+      <div class="flex flex-col gap-10 pt-10">
+        <%= for tier <- @tiers do %>
+          <.sponsor_segment tier={tier} sponsors={tier.companies} />
+        <% end %>
+      </div>
     </div>
     """
   end
 
   def sponsor_segment(assigns) do
     ~H"""
-    <div class="flex flex-col justify-center pt-10 sm:pt-20 lg:flex-row">
-      <div class="grid w-full grid-cols-1 place-items-center py-[5%] lg:py-0 lg:px-[10%]">
-        <p class="font-terminal uppercase pb-10 text-2xl text-accent lg:text-3xl">
-          {@tier}
+    <div class="flex flex-col justify-center">
+      <div class="flex w-full flex-col items-center justify-center">
+        <p class="uppercase font-light text-xl" style={"color: #{@tier.color}"}>
+          <span class="font-semibold">{@tier.name}</span> sponsors
         </p>
-        <div class="flex justify-center items-center flex-wrap gap-4 sm:p-6 lg:gap-10">
+        <div class="grid grid-cols-4 w-full lg:grid-cols-6 gap-x-12.5 gap-y-16 items-center justify-center pt-8">
           <%= for sponsor <- @sponsors |> Enum.shuffle() do %>
             <.link
               href={sponsor.url}
               target="_blank"
-              class="opacity-80 hover:opacity-100 hover:scale-105 duration-500 transition-all"
+              class="opacity-80 hover:opacity-100 hover:scale-105 duration-500 transition-all mx-auto"
             >
               <%= if sponsor.logo do %>
-                <div class="w-32 sm:w-64 py-4">
+                <div class="w-40 h-20 flex items-center justify-center">
                   <img
-                    class="w-full max-h-32 px-1 sm:px-4"
+                    class="max-w-full max-h-full object-contain"
                     src={Uploaders.Company.url({sponsor.logo, sponsor}, :original, signed: true)}
                   />
                 </div>
               <% else %>
-                <p class="text-2xl text-center p-2">
+                <p class="text-2xl text-center p-2 text-dark">
                   {sponsor.name}
                 </p>
               <% end %>

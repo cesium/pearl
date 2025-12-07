432d7942b9e09a784a9a6536288d52fc0bc89128
João Coelho <jhcoelho03@gmail.com>
2025-12-07 03:32:21 +0000

    fix: buy tickets route

--- lib/pearl_web/live/landing/components/footer.ex
+++ lib/pearl_web/live/landing/components/footer.ex
@@ -129,7 +129,7 @@ defmodule PearlWeb.Landing.Components.Footer do
   defp your_enei_links do
     [
       %{title: "Área Pessoal", url: "/users/log_in", enabled: true},
-      %{title: "Bilhetes e inscrição", url: "/buy", enabled: true},
+      %{title: "Bilhetes e inscrição", url: "#", enabled: true},
       %{
         title: "Survival Guide",
         url: "/docs/survival-guide.pdf",

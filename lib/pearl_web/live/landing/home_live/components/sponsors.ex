97386075b152f1e3e793d890208fac71b1ebfa3c
gxnca <fl11gg20@gmail.com>
2025-12-11 23:28:15 +0000

    fix: responsiveness

--- lib/pearl_web/live/landing/home_live/components/sponsors.ex
+++ lib/pearl_web/live/landing/home_live/components/sponsors.ex
@@ -7,11 +7,11 @@ defmodule PearlWeb.Landing.HomeLive.Components.Sponsors do
   def sponsors(assigns) do
     ~H"""
     <div class="flex items-center justify-center flex-col py-14.5 bg-[#EFEFED]">
-      <div class="flex flex-col gap-5 items-center text-black">
-        <h2 class="flex font-semibold justify-center text-center text-3xl w-[580px]">
+      <div class="flex flex-col gap-5 items-center text-black px-4 md:px-8">
+        <h2 class="flex font-semibold justify-center text-center text-2xl md:text-3xl max-w-full md:max-w-[580px]">
           {gettext("Um elenco de empresas que abre portas e janelas")}
         </h2>
-        <p class="lg:px-52.5 text-center">
+        <p class="text-center max-w-full md:max-w-3xl lg:max-w-5xl">
           As empresas incríveis que nos patrocinam são quem possibilita o ENEI. Durante todo o evento, vais poder encontrá-las e falar com os seus representantes para as conheceres melhor.
         </p>
       </div>
@@ -31,7 +31,7 @@ defmodule PearlWeb.Landing.HomeLive.Components.Sponsors do
         <p class="uppercase font-light text-xl" style={"color: #{@tier.color}"}>
           <span class="font-semibold">{@tier.name}</span> sponsors
         </p>
-        <div class="grid grid-cols-4 w-full lg:grid-cols-6 gap-x-12.5 gap-y-16 items-center justify-center pt-8">
+        <div class="grid grid-cols-2 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 w-full gap-x-8 gap-y-12 md:gap-x-12.5 md:gap-y-16 items-center justify-items-center pt-8 px-4">
           <%= for sponsor <- @sponsors |> Enum.shuffle() do %>
             <.link
               href={sponsor.url}
@@ -39,7 +39,7 @@ defmodule PearlWeb.Landing.HomeLive.Components.Sponsors do
               class="opacity-80 hover:opacity-100 hover:scale-105 duration-500 transition-all mx-auto"
             >
               <%= if sponsor.logo do %>
-                <div class="w-40 h-16 flex items-center justify-center">
+                <div class="w-30 h-16 sm:w-40 sm:h-16 flex items-center justify-center">
                   <img
                     class="max-w-full max-h-full object-contain"
                     src={Uploaders.Company.url({sponsor.logo, sponsor}, :original, signed: true)}

ae53019e8088c293da3ed2361b9065fee1a36325
jojocoelho <jhcoelho03@gmail.com>
2026-02-12 14:16:54 +0000

    fix: hero component

--- lib/pearl_web/live/landing/challenges_live/components/hero.ex
+++ lib/pearl_web/live/landing/challenges_live/components/hero.ex
@@ -10,33 +10,27 @@ defmodule PearlWeb.Landing.ChallengesLive.Components.Hero do
 
   def hero(assigns) do
     ~H"""
-    <div class="relative -mt-32">
-      <div class="absolute top-0 left-0 right-0 h-[600px] z-0 overflow-hidden">
-        <div class="absolute inset-0 bg-black"></div>
+    <section class="relative w-full bg-black overflow-hidden -mt-32">
+      <div class="hero-gradient-bg" aria-hidden="true"></div>
 
-        <div class="hero-gradient-bg"></div>
-
-        <div class="absolute -right-5 bottom-0 flex items-end">
-          <img
-            src={~p"/images/prizes.png"}
-            alt="Prizes"
-            class="h-[520px] md:h-[580px] w-auto object-contain"
-          />
-        </div>
-      </div>
-
-      <div class="relative z-10 w-full pt-64">
-        <div class="w-full mx-auto px-6.5 md:px-16 mb-20">
-          <h1 class="text-white text-4xl font-semibold mb-6">
+      <div class="relative z-10 flex flex-col md:flex-row md:items-center md:justify-between w-full md:min-h-[480px] pt-32 md:pt-8">
+        <div class="shrink-0 px-9 pt-8 md:w-1/2 ">
+          <h1 class="text-white text-3xl font-semibold mb-4 text-balance">
             {@title}
           </h1>
-
-          <p class="text-white/50 text-lg max-w-xl">
+          <p class="text-white/50 max-w-xl leading-relaxed">
             {@description}
           </p>
         </div>
+        <div class="relative shrink-0 md:w-1/2 flex items-end justify-center md:justify-end self-end">
+          <img
+            src={~p"/images/prizes.png"}
+            alt="Prizes"
+            class="w-full md:max-w-none md:w-auto h-auto max-h-[400px] xl:max-h-[520px] object-contain object-bottom"
+          />
+        </div>
       </div>
-    </div>
+    </section>
     """
   end
 end

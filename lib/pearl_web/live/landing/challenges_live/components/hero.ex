e4498e5932f7b0dd0febe20457bdac5029e492d6
jojocoelho <jhcoelho03@gmail.com>
2026-02-22 18:35:28 +0000

    fix: hero component

--- lib/pearl_web/live/landing/challenges_live/components/hero.ex
+++ lib/pearl_web/live/landing/challenges_live/components/hero.ex
@@ -11,8 +11,8 @@ defmodule PearlWeb.Landing.ChallengesLive.Components.Hero do
   def hero(assigns) do
     ~H"""
     <section class="relative w-full bg-black overflow-hidden -mt-32">
-      <div class="relative z-10 flex flex-col md:flex-row md:items-center md:justify-between w-full md:min-h-[480px] pt-32 md:pt-8">
-        <div class="shrink-0 px-7.5 md:px-12.5 pt-8 md:w-1/2 ">
+      <div class="relative z-10 flex flex-col lg:flex-row lg:items-center lg:justify-between w-full lg:min-h-[480px] pt-32 lg:pt-8">
+        <div class="shrink-0 px-7.5 lg:px-12.5 pt-8 lg:w-1/2">
           <h1 class="text-white text-3xl font-semibold mb-4 text-balance">
             {@title}
           </h1>
@@ -20,11 +20,11 @@ defmodule PearlWeb.Landing.ChallengesLive.Components.Hero do
             {@description}
           </p>
         </div>
-        <div class="relative shrink-0 md:w-1/2 flex items-end justify-center md:justify-end self-end">
+        <div class="relative shrink-0 lg:w-1/2 flex items-end justify-center lg:justify-end lg:self-end">
           <img
-            src={~p"/images/prizes.png"}
+            src={~p"/images/prizes.webp"}
             alt="Prizes"
-            class="w-full md:max-w-none md:w-auto h-auto max-h-[400px] xl:max-h-[520px] object-contain object-bottom"
+            class="h-full w-auto lg:max-w-none object-contain object-bottom"
           />
         </div>
       </div>

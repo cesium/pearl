74d0f7ccb7e7c6a756438492a98bd9f13dc35152
Nuno Miguel <30806591+nunom27@users.noreply.github.com>
2026-03-16 12:11:10 +0000

    fix: checkout ticket listing (#76)

--- lib/pearl_web/live/checkout/ticket_informations_live.ex
+++ lib/pearl_web/live/checkout/ticket_informations_live.ex
@@ -6,7 +6,7 @@ defmodule PearlWeb.Checkout.TicketInformationsLive do
   import PearlWeb.Components.Button
 
   def mount(_params, session, socket) do
-    ticket_types = TicketTypes.list_ticket_types()
+    ticket_types = TicketTypes.list_active_ticket_types()
 
     ticket_type_id =
       case Map.get(session, "ticket_type_id") do
@@ -49,6 +49,7 @@ defmodule PearlWeb.Checkout.TicketInformationsLive do
     changeset =
       %Ticket{}
       |> apply_step_validation(:choose_ticket, socket.assigns.ticket_data)
+      |> validate_ticket_active()
 
     if changeset.valid? do
       socket
@@ -66,6 +67,7 @@ defmodule PearlWeb.Checkout.TicketInformationsLive do
     changeset =
       %Ticket{}
       |> apply_step_validation(:precautions, socket.assigns.ticket_data)
+      |> validate_ticket_active()
 
     if changeset.valid? do
       socket
@@ -91,6 +93,7 @@ defmodule PearlWeb.Checkout.TicketInformationsLive do
     changeset =
       %Ticket{}
       |> apply_step_validation(:informations, socket.assigns.ticket_data)
+      |> validate_ticket_active()
 
     if changeset.valid? do
       socket
@@ -139,6 +142,7 @@ defmodule PearlWeb.Checkout.TicketInformationsLive do
     changeset =
       %Ticket{}
       |> apply_step_validation(socket.assigns.current_step, socket.assigns.ticket_data)
+      |> validate_ticket_active()
 
     if changeset.valid? do
       next_route = get_next_route(socket.assigns.current_step)
@@ -220,5 +224,15 @@ defmodule PearlWeb.Checkout.TicketInformationsLive do
 
   defp apply_step_validation(changeset, :conclusion, ticket_data) do
     changeset |> Tickets.change_ticket(ticket_data)
+  end
+
+  defp validate_ticket_active(changeset) do
+    changeset
+    |> Ecto.Changeset.validate_change(:ticket_type_id, fn :ticket_type_id, ticket_type_id ->
+      case Enum.find(TicketTypes.list_active_ticket_types(), fn tt -> tt.id == ticket_type_id end) do
+        nil -> [ticket_type_id: "Selected ticket type is not active"]
+        _ -> []
+      end
+    end)
   end
 end

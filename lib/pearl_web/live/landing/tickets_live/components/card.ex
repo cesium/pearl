4fa5506006ba5134d5cc48966b096c859a0fa170
GuilhermePSF <guilhermepsf23@gmail.com>
2026-03-19 00:59:36 +0000

    feat: split tickets page into event and activity sections with buy restrictions

--- lib/pearl_web/live/landing/tickets_live/components/card.ex
+++ lib/pearl_web/live/landing/tickets_live/components/card.ex
@@ -3,9 +3,27 @@ defmodule PearlWeb.Landing.TicketsLive.Components.Card do
   Tickets component.
   """
   use PearlWeb, :component
+  alias Pearl.Tickets
   import PearlWeb.Components.Button
 
+  defp button_state(ticket_type, user_ticket) do
+    cond do
+      ticket_type.type == :activity and (is_nil(user_ticket) or not Tickets.paid?(user_ticket)) ->
+        {:disabled, "Precisas de um passe geral"}
+
+      ticket_type.type == :event and not is_nil(user_ticket) and
+        user_ticket.ticket_type.priority >= ticket_type.priority and Tickets.paid?(user_ticket) ->
+        {:disabled, "Já tens um bilhete igual ou superior"}
+
+      true ->
+        :enabled
+    end
+  end
+
   def card(assigns) do
+    assigns =
+      assign(assigns, :button_state, button_state(assigns.ticket_type, assigns.user_ticket))
+
     ~H"""
     <div
       class="w-full min-h-[476px] h-full p-0.5"
@@ -32,21 +50,28 @@ defmodule PearlWeb.Landing.TicketsLive.Components.Card do
             </div>
           </div>
         </div>
-        <div class="flex gap-4 items-center justify-start">
-          <span class="text-black text-4xl font-extrabold">
-            {@ticket_type.price
-            |> Float.round(2)
-            |> :erlang.float_to_binary(decimals: 2)
-            |> String.replace(".", ",")}€
-          </span>
-          <div>
-            <.primary_button
-              phx-value-ticket_type_id={@ticket_type.id}
-              phx-click="select_ticket"
-              class="w-16 h-16 rounded-none bg-primary hover:bg-primary/90 cursor-pointer"
-              small
-            />
+        <div class="flex flex-col gap-2">
+          <div class="flex gap-4 items-center justify-start">
+            <span class="text-black text-4xl font-extrabold">
+              {@ticket_type.price
+              |> Float.round(2)
+              |> :erlang.float_to_binary(decimals: 2)
+              |> String.replace(".", ",")}€
+            </span>
+            <div>
+              <.primary_button
+                phx-value-ticket_type_id={@ticket_type.id}
+                phx-value-type={@ticket_type.type}
+                phx-click={if @button_state == :enabled, do: "select_ticket"}
+                disabled={@button_state != :enabled}
+                class={"size-16 rounded-none cursor-pointer #{if @button_state == :enabled, do: "bg-primary hover:bg-primary/90", else: "bg-dark/20 cursor-not-allowed"}"}
+                small
+              />
+            </div>
           </div>
+          <p :if={@button_state != :enabled} class="text-dark/50 text-sm">
+            {elem(@button_state, 1)}
+          </p>
         </div>
       </div>
     </div>
--- lib/pearl_web/live/landing/tickets_live/index.ex
+++ lib/pearl_web/live/landing/tickets_live/index.ex
@@ -4,21 +4,41 @@ defmodule PearlWeb.Landing.TicketsLive.Index do
   on_mount {PearlWeb.VerifyFeatureFlag, "tickets_enabled"}
 
   alias Pearl.TicketTypes
+  alias Pearl.Tickets
   import PearlWeb.Landing.TicketsLive.Components.Card
 
   def mount(_params, _session, socket) do
     ticket_types = TicketTypes.list_active_ticket_types()
+    event_ticket_types = Enum.filter(ticket_types, &(&1.type == :event))
+    activity_ticket_types = Enum.filter(ticket_types, &(&1.type == :activity))
 
-    {:ok, socket |> assign(:ticket_types, ticket_types) |> assign(:current_page, :tickets)}
+    user_ticket =
+      case socket.assigns.current_user do
+        nil -> nil
+        user -> Tickets.get_user_ticket(user.id)
+      end
+
+    {:ok,
+     socket
+     |> assign(:event_ticket_types, event_ticket_types)
+     |> assign(:activity_ticket_types, activity_ticket_types)
+     |> assign(:user_ticket, user_ticket)
+     |> assign(:current_page, :tickets)}
   end
 
   def handle_params(_unsigned_params, _uri, socket) do
     {:noreply, socket}
   end
 
+  def handle_event(
+        "select_ticket",
+        %{"ticket_type_id" => ticket_type_id, "type" => "activity"},
+        socket
+      ) do
+    {:noreply, redirect(socket, to: ~p"/checkout/activity/init?ticket_type_id=#{ticket_type_id}")}
+  end
+
   def handle_event("select_ticket", %{"ticket_type_id" => ticket_type_id}, socket) do
-    {:noreply,
-     socket
-     |> push_navigate(to: ~p"/checkout/init?ticket_type_id=#{ticket_type_id}")}
+    {:noreply, redirect(socket, to: ~p"/checkout/init?ticket_type_id=#{ticket_type_id}")}
   end
 end

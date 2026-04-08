492795136b8cea5bad849aad260a5ef8bc17dd1f
Enrico Prazeres <esprazj.dev@gmail.com>
2026-04-08 03:30:06 +0100

    feat: all golds task as a release function (DON'T MERGE THIS TO MAIN)

--- lib/pearl/release.ex
+++ lib/pearl/release.ex
@@ -5,7 +5,42 @@ defmodule Pearl.Release do
   """
   @app :pearl
 
+  import Ecto.Query, warn: false
+  alias Pearl.Repo
+
+  alias Pearl.Contest
+  alias Pearl.Contest.{Badge, BadgeRedeem}
+  alias Pearl.Accounts.Attendee
   alias Pearl.Accounts.Roles.Permissions
+
+    def run_gold_mine do
+    golds = ["Accenture", "AgentifAI", "Uphold", "Retail Consult"]
+
+    len = Kernel.length(golds)
+
+    attendees =
+      BadgeRedeem
+      |> join(:inner, [br], a in Attendee, on: br.attendee_id == a.id)
+      |> join(:inner, [br, a], b in Badge, on: br.badge_id == b.id)
+      |> where([br, a, b], b.name in ^golds)
+      |> group_by([br, a, b], a.id)
+      |> having([br, a, b], count(br.id) == ^len)
+      |> select([br, a, b], a)
+      |> subquery()
+      |> preload(:user)
+      |> Repo.all()
+
+    badge = Repo.one(from b in Badge, where: b.name == "Gold Mine")
+
+    for attendee <- attendees do
+      IO.puts("Processing #{attendee.user.name}")
+
+      case Contest.redeem_badge(badge, attendee) do
+        {:ok, _} -> IO.puts("Badge redeemed for #{attendee.user.name}")
+        {:error, _} -> IO.puts("Badge redeem failed for #{attendee.user.name}")
+      end
+    end
+  end
 
   def migrate do
     load_app()
